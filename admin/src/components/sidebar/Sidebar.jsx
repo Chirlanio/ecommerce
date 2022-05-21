ea05749e692eef3606bc539d2561a7c7d324155c
Chirlanio <fchirlanio@gmail.com>
2022-05-21 14:33:19 -0300

    Product List

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,19 @@
-import "./sidebar.css";
-import { AttachMoney, BarChart, ChatBubbleOutline, DynamicFeed, LineStyle, MailOutline, PersonOutline, ReportGmailerrorred, Storefront, Timeline, TrendingUp, WorkOutline } from "@mui/icons-material";
+import './sidebar.css'
+import {
+  AttachMoney,
+  BarChart,
+  ChatBubbleOutline,
+  DynamicFeed,
+  LineStyle,
+  MailOutline,
+  PersonOutline,
+  ReportGmailerrorred,
+  Storefront,
+  Timeline,
+  TrendingUp,
+  WorkOutline
+} from '@mui/icons-material'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   return (
@@ -8,73 +22,99 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyle className="sidebarIcon" />
-              Home
-            </li>
-            <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" />
-              Analytics
-            </li>
-            <li className="sidebarListItem">
-              <TrendingUp className="sidebarIcon" />
-              Sales
-            </li>
+            <Link to="/" className="link">
+              <li className="sidebarListItem active">
+                <LineStyle className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
+            <Link to="/analytics" className="link">
+              <li className="sidebarListItem">
+                <Timeline className="sidebarIcon" />
+                Analytics
+              </li>
+            </Link>
+            <Link to="/sales" className="link">
+              <li className="sidebarListItem">
+                <TrendingUp className="sidebarIcon" />
+                Sales
+              </li>
+            </Link>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <PersonOutline className="sidebarIcon" />
-              Users
-            </li>
-            <li className="sidebarListItem">
-              <Storefront className="sidebarIcon" />
-              Products
-            </li>
-            <li className="sidebarListItem">
-              <AttachMoney className="sidebarIcon" />
-              Transactions
-            </li>
-            <li className="sidebarListItem">
-              <BarChart className="sidebarIcon" />
-              Reports
-            </li>
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PersonOutline className="sidebarIcon" />
+                Users
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidebarListItem">
+                <Storefront className="sidebarIcon" />
+                Products
+              </li>
+            </Link>
+            <Link to="/transactions" className="link">
+              <li className="sidebarListItem">
+                <AttachMoney className="sidebarIcon" />
+                Transactions
+              </li>
+            </Link>
+            <Link to="/reports" className="link">
+              <li className="sidebarListItem">
+                <BarChart className="sidebarIcon" />
+                Reports
+              </li>
+            </Link>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Notifications</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <MailOutline className="sidebarIcon" />
-              Mail
-            </li>
-            <li className="sidebarListItem">
-              <DynamicFeed className="sidebarIcon" />
-              Feedback
-            </li>
-            <li className="sidebarListItem">
-              <ChatBubbleOutline className="sidebarIcon" />
-              Message
-            </li>
+            <Link to="/mail" className="link">
+              <li className="sidebarListItem">
+                <MailOutline className="sidebarIcon" />
+                Mail
+              </li>
+            </Link>
+            <Link to="/feedback" className="link">
+              <li className="sidebarListItem">
+                <DynamicFeed className="sidebarIcon" />
+                Feedback
+              </li>
+            </Link>
+            <Link to="/message" className="link">
+              <li className="sidebarListItem">
+                <ChatBubbleOutline className="sidebarIcon" />
+                Message
+              </li>
+            </Link>
           </ul>
         </div>
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Staff</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <WorkOutline className="sidebarIcon" />
-              Manage
-            </li>
-            <li className="sidebarListItem">
-              <Timeline className="sidebarIcon" />
-              Analytics
-            </li>
-            <li className="sidebarListItem">
-              <ReportGmailerrorred className="sidebarIcon" />
-              Reports
-            </li>
+            <Link to="/manage" className="link">
+              <li className="sidebarListItem">
+                <WorkOutline className="sidebarIcon" />
+                Manage
+              </li>
+            </Link>
+            <Link to="/userAnalytic" className="link">
+              <li className="sidebarListItem">
+                <Timeline className="sidebarIcon" />
+                Analytics
+              </li>
+            </Link>
+            <Link to="/reports" className="link">
+              <li className="sidebarListItem">
+                <ReportGmailerrorred className="sidebarIcon" />
+                Reports
+              </li>
+            </Link>
           </ul>
         </div>
       </div>
--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './topbar.css'
 import { NotificationsNone, Language, Settings } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
 
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
+          <Link to="/" className="link">
           <span className="logo">E-commerce Admin</span>
+          </Link>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
